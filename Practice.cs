91af1b07cf19437a53d4cf851e3a534db13725dd
mallanka <mallanka2@mail.ru>
2019-03-28 19:36:15 +0300

    AddConvertion

--- Practice.cs
+++ Practice.cs
@@ -13,25 +13,43 @@ namespace ConsoleApp7
 
             //1. CHAR CONVERSION 
             char sex = 'М';//to string
+            string sexString = Convert.ToString(sex);
             char place = '3';//to int
+            int placeInt = (int)Char.GetNumericValue(place);
             char hasFingerPrints = '0';//to boolean
+            bool hasFingerPrintsBool = Convert.ToBoolean((int)Char.GetNumericValue(hasFingerPrints));
 
             //2. STRING CONVERSION           
             string hasPhotoString = "True";//to bolean
+            bool hasPhotoStringBool = Convert.ToBoolean(hasPhotoString);
             string flatNumber = "34";//to int
+            int flatNumberInt = Convert.ToInt32(flatNumber);
             string visaPriceString = "34,23";//to float
+            float visaPriceStringFloat = (float)(Convert.ToDouble(visaPriceString));
             string photoPriceString = "7.23";//to float
+            float photoPriceStringFloat = (float)(Convert.ToDouble(photoPriceString, System.Globalization.CultureInfo.InvariantCulture));
 
             //3. BOOL CONVERSION
             bool hasFree2Pages = false;//to string, to char, to int
+            string hasFree2PagesString = Convert.ToString(hasFree2Pages);
+            int hasFree2PagesInt = Convert.ToInt32(hasFree2Pages);
+            char hasFree2PagesChar = Convert.ToChar(hasFree2PagesInt);
 
             //4. DECIMAL CONVERSION
             double visaPrice = 60;//to int, to string
+            int visaPriceInt = (int)visaPrice;
+            string DecimalVisaPriceString = Convert.ToString(visaPrice);
             double finterPrintsPrice = 55.2;//to int, to string
+            int finterPrintsPriceInt = (int)finterPrintsPrice;
+            string finterPrintsPriceString = Convert.ToString(finterPrintsPrice);
 
             //4. INT CONVERSION
             int birthYear = 2000;//to string, to double, to char
+            string birthYearString = Convert.ToString(birthYear);
+            double birthYearDouble = birthYear;
+            char birthYearChar = (char)birthYear;
             int hasPhotoInt = 1;//to boolean
+            bool hasPhotoBool = Convert.ToBoolean(hasPhotoInt);
         }
     }
 }
